3bc16b097c2377d78741df576ac33396ac78cd51
ecuadros <ecuadros@spc.org.pe>
2025-09-10 11:08:58 -0500

    PC1 para el 17 Sep

--- DemoVector.cpp
+++ DemoVector.cpp
@@ -6,8 +6,8 @@ using namespace std;
 void DemoVector(){
     // CVector vector(10);
     // vector.insert(5);
-    // // TODO habilitar el uso de []
+    // TODO  (Nivel 1) habilitar el uso de []
     // vector[3] = 8;
-    // // TODO habilitar que el vector pueda ser escrito con cout <<
+    // TODO  (Nivel 2) habilitar que el vector pueda ser escrito con cout <<
     // cout << vector << endl;
 }
--- vector.h
+++ vector.h
@@ -1,20 +1,36 @@
 #ifndef __VECTOR_H__
 #define __VECTOR_H__
 
-// TODO: Agregar Traits
+// PC1: deben hacer:
+//      2 problemas de nivel 2
+//      3 problemas de nivel 1
+// Cada solucion enviarla como un Pull request
 
-// TODO: Agregar Iterators (forward, backward)
+// TODO (Nivel 2): Agregar Traits
 
-// TODO: Agregar Documentacion para gebnerar con doxygen
+// TODO (Nivel 2): Agregar Iterators (forward, backward)
 
+// TODO (Nivel 1): Agregar Documentacion para generar con doxygen
+
+// TODO  (Nivel 2): Agregar control de concurrencia en todo el vector
 template <typename T>
 class CVector{
    
-    T *m_pVect = nullptr;
+    T      *m_pVect = nullptr;
+    size_t  m_count = 0; // How many elements we have now?
+    size_t  m_max   = 0; // Max capacity
 public:
-    CVector(size_t n);
-    void insert(T &elem);
+    // TODO  (Nivel 1) Agregar un constructor por copia
+    CVector(CVector &v);
 
+    CVector(size_t n);
+    // TODO  (Nivel 2): Agregar un move constructor
+    CVector(CVector &&v);
+
+    // TODO: (Nivel 1) implementar el destructor de forma segura
+    vrtual CVector();
+    void insert(T &elem);
+    void resize();
 };
 
 template <typename T>
@@ -22,12 +38,23 @@ CVector<T>::CVector(size_t n){
 
 }
 
-// TODO: la funcion insert debe permitir que el vector crezca si ha desbordado
-// TODO: insert debe soportar concurrencia
+// TODO (Nivel 1): hacer dinamico el delta de crecimiento
 template <typename T>
-void CVector<T>::insert(T &elem)
-{
+void CVector<T>::resize(){
+    T *pTmp = new T[m_max+10];
+    for(auto i=0; i < m_max ; ++i)
+        pTmp[i] = m_pVect[i];
+    delete [] m_pVect;
+    m_max += 10;
+    m_pVect = pTmp;
+}
 
+// TODO (ya está hecha): la funcion insert debe permitir que el vector crezca si ha desbordado
+template <typename T>
+void CVector<T>::insert(T &elem){
+    if(m_count == m_max)
+        resize();
+    m_pVect[m_count++] = elem;
 }
 
 #endif // __VECTOR_H__
