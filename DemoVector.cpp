4d834265c5f8e344aa5c966b9de6bcd8c11acf7d
CarlosCCQ <jhan.ccanto.q@uni.pe>
2025-09-15 13:01:37 -0500

    Implementacion de DemoVector.cpp

--- DemoVector.cpp
+++ DemoVector.cpp
@@ -10,4 +10,12 @@ void DemoVector(){
     // vector[3] = 8;
     // TODO  (Nivel 2) habilitar que el vector pueda ser escrito con cout <<
     // cout << vector << endl;
+
+    CVector<int> vector(10);
+    int val1 = 5, val2 = 8, val3 = 12;
+    vector.insert(val1);
+    vector.insert(val2);
+    vector.insert(val3);
+    vector[0] = 100;
+    cout << vector << endl;
 }
