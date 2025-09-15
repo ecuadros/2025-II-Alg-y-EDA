cf70e48ef1f1b4a6f29e2df4e1011972a5e38146
CarlosCCQ <jhan.ccanto.q@uni.pe>
2025-09-15 02:08:23 -0500

    implementacion correcta de CVector(size_t n)

--- vector.h
+++ vector.h
@@ -14,11 +14,12 @@
 
 // TODO  (Nivel 2): Agregar control de concurrencia en todo el vector
 template <typename T>
-class CVector{
-   
-    T      *m_pVect = nullptr;
-    size_t  m_count = 0; // How many elements we have now?
-    size_t  m_max   = 0; // Max capacity
+class CVector
+{
+
+    T *m_pVect = nullptr;
+    size_t m_count = 0; // How many elements we have now?
+    size_t m_max = 0;   // Max capacity
 public:
     // TODO  (Nivel 1) Agregar un constructor por copia
     CVector(CVector &v);
@@ -34,22 +35,21 @@ public:
 };
 
 template <typename T>
-CVector<T>::CVector(size_t n){
-    if(n==0){
-        m_pVect = nullptr;
-        m_max = 0;
-        m_count = 0;
-    } else {
+CVector<T>::CVector(size_t n) : m_pVect(nullptr), m_count(0), m_max(0)
+{
+    if (n > 0)
+    {
         m_pVect = new T[n];
         m_max = n;
-        m_count = 0;
     }
 }
 
 // Implementacion del destructor de forma segura
 template <typename T>
-CVector<T>::~CVector(){
-    if(m_pVect != nullptr){
+CVector<T>::~CVector()
+{
+    if (m_pVect != nullptr)
+    {
         delete[] m_pVect;
         m_pVect = nullptr;
     }
@@ -57,19 +57,21 @@ CVector<T>::~CVector(){
 
 // TODO (Nivel 1): hacer dinamico el delta de crecimiento
 template <typename T>
-void CVector<T>::resize(){
-    T *pTmp = new T[m_max+10];
-    for(auto i=0; i < m_max ; ++i)
+void CVector<T>::resize()
+{
+    T *pTmp = new T[m_max + 10];
+    for (auto i = 0; i < m_max; ++i)
         pTmp[i] = m_pVect[i];
-    delete [] m_pVect;
+    delete[] m_pVect;
     m_max += 10;
     m_pVect = pTmp;
 }
 
 // TODO (ya está hecha): la funcion insert debe permitir que el vector crezca si ha desbordado
 template <typename T>
-void CVector<T>::insert(T &elem){
-    if(m_count == m_max)
+void CVector<T>::insert(T &elem)
+{
+    if (m_count == m_max)
         resize();
     m_pVect[m_count++] = elem;
 }
