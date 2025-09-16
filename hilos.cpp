afe012bcc57a38cba8650d644144c6b60448cedd
Max <oscar.torres.m@uni.pe>
2025-09-16 15:32:57 -0500

    Revert "Mutex added"
    This reverts commit ad59222b287efb3be8727305b6cfdd10aa4d302b.

--- hilos.cpp
+++ hilos.cpp
@@ -5,8 +5,6 @@
 #include "vector.h"
 using namespace std;
 
-mutex cout_mutex;
-
 // Function to be run by the thread
 void func(int thread_id, CVector<int> &v) {
     cout << "Hello from the thread!" << endl;
