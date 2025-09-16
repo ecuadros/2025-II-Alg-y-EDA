9128d29af2dcd370ec9912efee6675c51c608498
Max <oscar.torres.m@uni.pe>
2025-09-16 16:09:55 -0500

    Dynamic Rezise added

--- DemoVector.cpp
+++ DemoVector.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void DemoVector(){
     CVector<int> vector(10);
-    // vector.insert(5);
+    vector.insert(5);
     // TODO  (Nivel 1) habilitar el uso de []
     // vector[3] = 8;
     // TODO  (Nivel 2) habilitar que el vector pueda ser escrito con cout <<
--- vector.h
+++ vector.h
@@ -15,97 +15,111 @@
 // TODO  (Nivel 2): Agregar control de concurrencia en todo el vector
 template <typename T>
 class CVector{
-   
-    T      *m_pVect = nullptr;
-    size_t  m_count = 0; // How many elements we have now?
-    size_t  m_max   = 0; // Max capacity
+	T      *m_pVect = nullptr;
+	size_t  m_count = 0; // How many elements we have now?
+	size_t  m_max   = 0; // Max capacity
+	size_t m_currentDelta = 0;
 public:
-    // TODO  (Nivel 1) Agregar un constructor por copia
-    CVector(CVector &v);
+	
+// TODO  (Nivel 1) Agregar un constructor por copia
+	CVector(CVector &v);
 
-    CVector(size_t n);
-    // TODO  (Nivel 2): Agregar un move constructor
-    CVector(CVector &&v);
+	CVector(size_t n);
 
-    // TODO: (Nivel 1) implementar el destructor de forma segura
-    virtual ~CVector();
-    void insert(T &elem);
-    T&   operator[](size_t index);
-    size_t size() const { return m_count; }
+	// TODO  (Nivel 2): Agregar un move constructor
+	CVector(CVector &&v);
+
+	// TODO: (Nivel 1) implementar el destructor de forma segura
+	virtual ~CVector();
+	
+	void insert(const T &elem);
+	
+	T&   operator[](size_t index);
+	
+	size_t size() const { return m_count; }
+
 private:
-    void resize();
-    void Init(size_t n);
-    void Destroy();
+
+	void resize(size_t delta = 0);
+	void Destroy();
 };
 
 template <typename T>
 CVector<T>::CVector(size_t n){
-    Init(n);
+	m_pVect = new T[n];
+	m_max = n;
+	m_currentDelta = n;
 }
 
 template <typename T>
 CVector<T>::CVector(CVector &v) 
-          : m_max(v.m_max), 
-            m_count(v.m_count) {
-    if (m_max > 0)
-        m_pVect = new T[m_max];
-    for (size_t i = 0; i < m_count; ++i)
-        m_pVect[i] = v[i];       
-}
+	: m_max(v.m_max), m_count(v.m_count) {
 
-// TODO (Nivel 1): hacer dinamico el delta de crecimiento
-template <typename T>
-void CVector<T>::resize(){
-    T *pTmp = new T[m_max+10];
-    for(auto i=0; i < m_max ; ++i)
-        pTmp[i] = m_pVect[i];
-    delete [] m_pVect;
-    m_max += 10;
-    m_pVect = pTmp;
+	if (m_max > 0)
+		m_pVect = new T[m_max];
+	for (size_t i = 0; i < m_count; ++i)
+		m_pVect[i] = v[i];       
 }
 
 template <typename T>
-void CVector<T>::Init(size_t n){
-    Destroy();
-    resize();
-}
+void CVector<T>::resize(size_t delta) {
+	size_t auxDelta = m_currentDelta;
+
+	if(delta != 0){
+		if(delta < m_currentDelta) auxDelta = delta;
+		else {
+			m_currentDelta = delta;
+			auxDelta = m_currentDelta;
+		}
+	}
+
+	T *pTmp = new T[m_max + auxDelta];
+
+	for(size_t i = 0; i < m_max; ++i)
+		pTmp[i] = m_pVect[i];
+
+	delete [] m_pVect;
+	m_max += auxDelta;
+
+	m_pVect = pTmp;
+};
 
 template <typename T>
-    CVector<T>::~CVector(){
-    Destroy();
+	CVector<T>::~CVector(){
+	Destroy();
 }
 
 template <typename T>
 void CVector<T>::Destroy(){
-    m_count = 0; 
-    m_max   = 0;
-    delete [] m_pVect;
-    m_pVect = nullptr;
+	m_count = 0; 
+	m_max   = 0;
+	delete [] m_pVect;
+	m_pVect = nullptr;
 }
 
 // TODO (ya está hecha): la funcion insert debe permitir que el vector crezca si ha desbordado
 template <typename T>
-void CVector<T>::insert(T &elem){
-    if(m_count == m_max)
-        resize();
-    m_pVect[m_count++] = elem;
+void CVector<T>::insert(const T &elem){
+	if(m_count == m_max)
+		resize();
+	m_pVect[m_count++] = elem;
 }
 
 template <typename T>
 T& CVector<T>::operator[](size_t index) {
-    if (index >= m_count) {
-        throw std::out_of_range("Index out of range");
-    }
-    return m_pVect[index];
+	if (index >= m_count) {
+		throw std::out_of_range("Index out of range");
+	}
+	return m_pVect[index];
 }
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, CVector<T>& vec) {
-    // os << "[";
-    for (size_t i = 0; i < vec.size(); ++i)
-        os << vec[i] << " ";
-    // os << "]";
-    return os;
+	// os << "[";
+	for (size_t i = 0; i < vec.size(); ++i)
+		os << vec[i] << " ";
+	// os << "]";
+	return os;
 }
 
 #endif // __VECTOR_H__
