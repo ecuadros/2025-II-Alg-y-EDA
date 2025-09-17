bdc37807add90c0ba1ed3e81c7aa7766d2195eae
Alex Vega <axvg@users.noreply.github.com>
2025-09-16 23:49:51 -0500

    TODO: delta dinamico done

--- vector.h
+++ vector.h
@@ -24,15 +24,17 @@ public:
     CVector(CVector &v);
 
     CVector(size_t n);
-    CVector(CVector &v);
+    // CVector(CVector &v);
     // TODO  (Nivel 2): Agregar un move constructor
     CVector(CVector &&v);
 
     // TODO: (Nivel 1) implementar el destructor de forma segura
     virtual ~CVector();
-    void insert(T &elem);
+    void insert(const T &elem);
     T&   operator[](size_t index);
     size_t size() const { return m_count; }
+    size_t  m_growth_factor = 2;
+
 private:
     void resize();
     void Init(size_t n);
@@ -54,14 +56,14 @@ CVector<T>::CVector(CVector &v)
         m_pVect[i] = v[i];       
 }
 
-// TODO (Nivel 1): hacer dinamico el delta de crecimiento
 template <typename T>
 void CVector<T>::resize(){
-    T *pTmp = new T[m_max+10];
-    for(auto i=0; i < m_max ; ++i)
+    size_t new_capacity = (m_max == 0) ? 1 : m_max * m_growth_factor;
+    T *pTmp = new T[new_capacity];
+    for(auto i=0; i < m_count ; ++i)
         pTmp[i] = m_pVect[i];
     delete [] m_pVect;
-    m_max += 10;
+    m_max = new_capacity;
     m_pVect = pTmp;
 }
 
@@ -86,7 +88,7 @@ void CVector<T>::Destroy(){
 
 // TODO (ya está hecha): la funcion insert debe permitir que el vector crezca si ha desbordado
 template <typename T>
-void CVector<T>::insert(T &elem){
+void CVector<T>::insert(const T &elem){
     if(m_count == m_max)
         resize();
     m_pVect[m_count++] = elem;
