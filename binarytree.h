e7e8cdfb98c7eae83adbb75afd4d1cabb6d06981
janiopi <janio072003@hotmail.com>
2025-10-11 14:51:21 -0500

    feat: Variadic templates version for preorder

--- binarytree.h
+++ binarytree.h
@@ -198,11 +198,16 @@ public:
     // TODO: Generalize this function to apply any function
     void preorder (ostream &os)    {   preorder (m_pRoot, 0, os);  }
     // TODO: Generalize this function to apply any function
-    void preorder(Node  *pNode, size_t level, ostream &os){
+    template <typename Function, typename... Args>
+    void preorder(Function func, Args const&... args){
+        preorder(m_pRoot,0,func,args...);
+    }
+    template <typename Function, typename... Args>
+    void preorder(Node  *pNode, size_t level, Function func, Args const&... args){
         if( pNode ){   
-            os << " --> " << pNode->getDataRef();
-            preorder(pNode->getChild(0), level+1, os);
-            preorder(pNode->getChild(1), level+1, os);            
+            func(pNode->getDataRef(), level, args...);
+            preorder(pNode->getChild(0), level+1, func, args...);
+            preorder(pNode->getChild(1), level+1, func, args...);
         }
     }
 
