418b7c8ecc77f16ec448e133ee6098d65e855cff
Omar Vite <omar.vite@culqi.com>
2025-10-11 12:33:07 -0500

    feat: confirm ascending/descending support via Traits

--- doublelinkedlist.h
+++ doublelinkedlist.h
@@ -92,7 +92,6 @@ class backward_double_linkedlist_iterator{
      value_type &operator*(){    return m_pNode->GetDataRef();   }
 };
 
-// TODO Agregar que sea ascendente o descendente con el mismo codigo
 template <typename Traits>
 class CDoubleLinkedList{
 public:
