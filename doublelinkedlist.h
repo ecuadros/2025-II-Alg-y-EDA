ed295f26e3a370c3ae0a0d5a14ff427ff875aff2
Omar Vite <omar.vite@culqi.com>
2025-10-11 12:29:47 -0500

    feat: implement destructor with memory cleanup

--- doublelinkedlist.h
+++ doublelinkedlist.h
@@ -192,10 +192,15 @@ CDoubleLinkedList<Traits>::CDoubleLinkedList(CDoubleLinkedList &&other){
     m_fCompare = std::move(other.m_fCompare);
 }
 
-// TODO: Implementar y liberar la memoria de cada Node
 template <typename Traits>
 CDoubleLinkedList<Traits>::~CDoubleLinkedList()
 {
+    Node *pCurrent = m_pRoot;
+    while(pCurrent){
+        Node *pNext = pCurrent->GetNext();
+        delete pCurrent;
+        pCurrent = pNext;
+    }
 }
 
 void DemoDoubleLinkedList();
