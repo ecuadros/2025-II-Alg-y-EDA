b61c796f5b783db1d0e40440591f5b4504779774
Omar Vite <omar.vite@culqi.com>
2025-10-13 23:30:44 -0500

    feat: implement AVL tree balancing and rotations
    - Add balance factor tracking to CAVLNode
    - Implement left and right rotation methods
    - Add height calculation and balance factor computation
    - Implement four rotation cases in internal_insert
    - Override CreateNode to instantiate AVL nodes
    - Fix trait definitions for proper type recursion

--- avl.h
+++ avl.h
@@ -4,52 +4,137 @@
 #include "binarytree.h"
 
 template <typename Traits>
-class CAVLNode : public CBinaryTreeNode<value_type>{
+class CAVLNode : public CBinaryTreeNode<Traits>{
 public:
   using value_type = typename Traits::T;
-  using Node       = CBinaryTreeNode<value_type>;
+  using BaseNode   = CBinaryTreeNode<Traits>;
+  using Node       = CAVLNode<Traits>;
+
 protected:
-    int     m_balanceFactor = 0; // Balance factor for AVL tree
+    int m_balanceFactor = 0;
+
 public:
+    CAVLNode(Node* pParent, value_type data, Ref ref, Node* p0 = nullptr, Node* p1 = nullptr)
+        : BaseNode(pParent, data, ref, p0, p1), m_balanceFactor(0) {}
+
+    int getBalanceFactor() const { return m_balanceFactor; }
+    void setBalanceFactor(int bf) { m_balanceFactor = bf; }
 };
 
 template <typename _T>
 struct AVLAscTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = less<T>;
+    using  T         = _T;
+    using  Node      = CAVLNode<AVLAscTraits<_T>>;
+    using  CompareFn = less<_T>;
 };
 
 template <typename _T>
 struct AVLDescTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = greater<T>;
+    using  T         = _T;
+    using  Node      = CAVLNode<AVLDescTraits<_T>>;
+    using  CompareFn = greater<_T>;
 };
 
 template <typename Traits>
 class CAVLTree : public CBinaryTree<Traits> {
 public:
     using Base       = CBinaryTree<Traits>;
+    using BaseNode   = typename Base::Node;
     using Node       = typename Traits::Node;
-    using value_type = typename Traits::value_type;  
+    using value_type = typename Traits::T;
     using CompareFn  = typename Traits::CompareFn;
     using Container  = CAVLTree<Traits>;
     using iterator   = binary_tree_iterator<Container>;
 
 protected:
-    // Additional members for AVL tree balancing can be added here
-    Node *internal_insert(value_type &elem, Ref ref,
+    int getHeight(Node* pNode) {
+        if (!pNode) return 0;
+        int leftHeight = getHeight((Node*)pNode->getChild(0));
+        int rightHeight = getHeight((Node*)pNode->getChild(1));
+        return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+    }
+
+    int getBalanceFactor(Node* pNode) {
+        if (!pNode) return 0;
+        return getHeight((Node*)pNode->getChild(0)) - getHeight((Node*)pNode->getChild(1));
+    }
+
+    Node* rotateRight(Node* y) {
+        Node* x = (Node*)y->getChild(0);
+        Node* T2 = (Node*)x->getChild(1);
+
+        x->m_pChild[1] = y;
+        y->m_pChild[0] = T2;
+
+        x->m_pParent = y->m_pParent;
+        y->m_pParent = x;
+        if (T2) T2->m_pParent = y;
+
+        y->setBalanceFactor(getBalanceFactor(y));
+        x->setBalanceFactor(getBalanceFactor(x));
+
+        return x;
+    }
+
+    Node* rotateLeft(Node* x) {
+        Node* y = (Node*)x->getChild(1);
+        Node* T2 = (Node*)y->getChild(0);
+
+        y->m_pChild[0] = x;
+        x->m_pChild[1] = T2;
+
+        y->m_pParent = x->m_pParent;
+        x->m_pParent = y;
+        if (T2) T2->m_pParent = x;
+
+        x->setBalanceFactor(getBalanceFactor(x));
+        y->setBalanceFactor(getBalanceFactor(y));
+
+        return y;
+    }
+
+    Node* CreateNode(Node* pParent, value_type elem, Ref ref) {
+        return new Node(pParent, elem, ref);
+    }
+
+    Node* internal_insert(value_type &elem, Ref ref,
                           Node* pParent, Node*& rpOrigin) override
     {
-        // Call base class insert
-        Node* newNode = Base::internal_insert(elem, ref, pParent, rpOrigin);
-        // TODO: Verificar balance y realizar rotaciones si es necesario
-        
-        return newNode;
+        if (!rpOrigin) {
+            ++Base::m_size;
+            return (rpOrigin = CreateNode(pParent, elem, ref));
+        }
+
+        CompareFn Compfn;
+        size_t branch = Compfn(elem, rpOrigin->getDataRef()) ? 0 : 1;
+        rpOrigin->m_pChild[branch] = internal_insert(elem, ref, rpOrigin, (Node*&)rpOrigin->m_pChild[branch]);
+
+        int balance = getBalanceFactor(rpOrigin);
+
+        if (balance > 1 && Compfn(elem, ((Node*)rpOrigin->getChild(0))->getDataRef())) {
+            return rotateRight(rpOrigin);
+        }
+
+        if (balance < -1 && !Compfn(elem, ((Node*)rpOrigin->getChild(1))->getDataRef())) {
+            return rotateLeft(rpOrigin);
+        }
+
+        if (balance > 1 && !Compfn(elem, ((Node*)rpOrigin->getChild(0))->getDataRef())) {
+            rpOrigin->m_pChild[0] = rotateLeft((Node*)rpOrigin->getChild(0));
+            return rotateRight(rpOrigin);
+        }
+
+        if (balance < -1 && Compfn(elem, ((Node*)rpOrigin->getChild(1))->getDataRef())) {
+            rpOrigin->m_pChild[1] = rotateRight((Node*)rpOrigin->getChild(1));
+            return rotateLeft(rpOrigin);
+        }
+
+        rpOrigin->setBalanceFactor(getBalanceFactor(rpOrigin));
+        return rpOrigin;
     }
+
 public:
-    CAVLTree() : Base() {} // Empty tree
+    CAVLTree() : Base() {}
 
 };
 
