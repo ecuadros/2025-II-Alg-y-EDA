c09ec4c5f06345ea18687122e042d1514b2328aa
Cris2804 <milagros.ruiz.a@uni.pe>
2025-10-14 03:50:53 -0500

    Generalizar preorden y postorden  binary tree correccion

--- binarytree.h
+++ binarytree.h
@@ -42,56 +42,13 @@ public:
     value_type       &getDataRef()             {   return m_data;    }
     const value_type &getDataRef()        const{   return m_data;    }
 
-    // preorder 
-    template <typename F, typename... Args>
-    void preorder(F&& f, Args&&... args) {
-        preorder_impl(m_pRoot, 0, std::forward<F>(f), std::forward<Args>(args)...);
-    }
-
-    //postorder
-    template <typename F, typename... Args>
-    void postorder(F&& f, Args&&... args) {
-        postorder_impl(m_pRoot, 0, std::forward<F>(f), std::forward<Args>(args)...);
-    }
+    
 
 
 
 
 protected: // TODO Hecho : Add this class as friend of the BinaryTree
         // and make these methods private
-
-    template <typename F, typename... Args>
-    static void call_visit(F&& f, Node* n, size_t level, Args&&... args) {
-        if constexpr (std::is_invocable_v<F, Node*, size_t, Args...>) {
-            std::invoke(std::forward<F>(f), n, level, std::forward<Args>(args)...);
-        } else if constexpr (std::is_invocable_v<F, value_type&, size_t, Args...>) {
-            std::invoke(std::forward<F>(f), n->getDataRef(), level, std::forward<Args>(args)...);
-        } else if constexpr (std::is_invocable_v<F, value_type&, Args...>) {
-            std::invoke(std::forward<F>(f), n->getDataRef(), std::forward<Args>(args)...);
-        } else if constexpr (std::is_invocable_v<F, Node*, Args...>) {
-            std::invoke(std::forward<F>(f), n, std::forward<Args>(args)...);
-        } else {
-            static_assert([]{return false;}(), "Visitor con firma no soportada");
-        }
-    }
-
-    // Implementación recursiva de preorden
-    template <typename F, typename... Args>
-    static void preorder_impl(Node* n, size_t level, F&& f, Args&&... args) {
-        if (!n) return;
-        call_visit(std::forward<F>(f), n, level, std::forward<Args>(args)...);
-        preorder_impl(n->getChild(0), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
-        preorder_impl(n->getChild(1), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
-    }
-
-    // Implementación recursiva de posorden
-    template <typename F, typename... Args>
-    static void postorder_impl(Node* n, size_t level, F&& f, Args&&... args) {
-        if (!n) return;
-        postorder_impl(n->getChild(0), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
-        postorder_impl(n->getChild(1), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
-        call_visit(std::forward<F>(f), n, level, std::forward<Args>(args)...);
-    }
 
     void      setupChild( Node *pChild, size_t pos)  {   m_pChild[pos] = pChild;  }
     Node    * getChild(size_t branch){ return m_pChild[branch];  }
@@ -124,7 +81,7 @@ public:
 template <typename _T>
 struct BinaryTreeAscTraits{
     using  T         = _T;
-    using  Node      = CBinaryTreeNode<T>;
+    using  Node      = CBinaryTreeNode<BinaryTreeAscTraits<_T>>;
     using  CompareFn = less<T>;
 };
 
@@ -132,7 +89,7 @@ template <typename _T>
 struct BinaryTreeDescTraits
 {
     using  T         = _T;
-    using  Node      = CBinaryTreeNode<T>;
+    using  Node      = CBinaryTreeNode<BinaryTreeDescTraits<_T>>;
     using  CompareFn = greater<T>;
 };
 
@@ -150,6 +107,7 @@ protected:
     Node    *m_pRoot = nullptr;
     size_t   m_size  = 0;
     CompareFn Compfn{};
+
 public: 
     CBinaryTree() = default;
     CBinaryTree(const CBinaryTree&) = delete; //disable copy
@@ -186,16 +144,35 @@ public:
 
     size_t  size()  const       { return m_size;       }
     bool    empty() const       { return size() == 0;  }
+    
     // TODO: insert must receive two paramaters: elem and Ref value
     void insert(value_type& elem, Ref ref) {
-        m_pRoot = internal_insert(elem, ref, nullptr, nullptr, m_pRoot);
+        m_pRoot = internal_insert(elem, ref, nullptr, m_pRoot);
     }
 
+    //Preorder 
+    template <typename F, typename... Args>
+    void preorder(F&& f, Args&&... args) {
+        preorder_impl(m_pRoot, 0, std::forward<F>(f), std::forward<Args>(args)...);
+    }
+
+    //postorder
+    template <typename F, typename... Args>
+    void postorder(F&& f, Args&&... args) {
+        postorder_impl(m_pRoot, 0, std::forward<F>(f), std::forward<Args>(args)...);
+    }
+
+    void preorder_print(std::ostream& os) { preorder([&](value_type& x){ os << " --> " << x; }); }
+    void postorder_print(std::ostream& os){ postorder([&](value_type& x){ os << " --> " << x; }); }
+
+
+
+
 protected:
-    Node* CreateNode(Node* pParent, value_type elem, Ref ref) {
+    Node* CreateNode(Node* pParent,const value_type elem, Ref ref) {
         return new Node(pParent, elem, ref);
     }
-    virtual Node* internal_insert(value_type &elem, Ref ref,
+    virtual Node* internal_insert(const value_type &elem, Ref ref,
                                   Node* pParent, Node*& rpOrigin)
     {
         if (!rpOrigin) {
@@ -203,109 +180,57 @@ protected:
             return (rpOrigin = CreateNode(pParent, elem, ref));
         }
 
-        size_t branch = Compfn(elem, rpOrigin->getDataRef()) ? 0 : 1;
-        Node *pNode = internal_insert(elem, ref, nullptr, rpOrigin, rpOrigin->getChildRef(branch));
-        return pNode;
+        const bool goLeft = Compfn(elem, rpOrigin->getDataRef());
+        Node*& next = rpOrigin->getChildRef(goLeft ? 0 : 1);
+        return internal_insert(elem, ref, rpOrigin, next);
     }
-public:
-    CBinaryTree(){} // Empty tree
-    
-    // TODO: Copy Constructor. We have duplicate each node
-    CBinaryTree(Binary &other);
-    
-    // Move Constructor
-    CBinaryTree(Binary &&other)
-        : m_pRoot(std::exchange(other.m_pRoot, nullptr)), 
-          m_size (std::exchange(other.m_size, 0)), 
-          Compfn (std::exchange(other.Compfn, nullptr))
-    { }
 
-    // TODO: Recursivo y seguro. Destruir Nodes recursivamente
-    virtual ~CBinaryTree(){  } 
-    
-    // TODO: Generalizar estos recorridos para recibir cualquier funcion
-    // con una cantidad flexible de parametros con variadic templates
-    // Google: C++ parameter packs cplusplus
-        void inorder  (ostream &os)    {   inorder  (m_pRoot, 0, os);  }
-    // TODO: 
-    void inorder(Node  *pNode, size_t level, ostream &os){
-        if( pNode ){
-            //Node *pParent = pNode->getParent();
-            inorder(pNode->getChild(0), level+1, os);
-            os << " --> " << pNode->getDataRef();
-            inorder(pNode->getChild(1), level+1, os);
+    // Llamador generico que adapta la llamada al visitor
+    template <typename F, typename... Args>
+    static void call_visit(F&& f, Node* n, size_t level, Args&&... args) {
+        if constexpr (std::is_invocable_v<F, Node*, size_t, Args...>) {
+            std::invoke(std::forward<F>(f), n, level, std::forward<Args>(args)...);
+        } else if constexpr (std::is_invocable_v<F, value_type&, size_t, Args...>) {
+            std::invoke(std::forward<F>(f), n->getDataRef(), level, std::forward<Args>(args)...);
+        } else if constexpr (std::is_invocable_v<F, value_type&, Args...>) {
+            std::invoke(std::forward<F>(f), n->getDataRef(), std::forward<Args>(args)...);
+        } else if constexpr (std::is_invocable_v<F, Node*, Args...>) {
+            std::invoke(std::forward<F>(f), n, std::forward<Args>(args)...);
+        } else {
+            static_assert([]{return false;}(), "Visitor con firma no soportada");
         }
     }
 
-    // TODO: Generalize this function by using iterators and apply any function
-    void inorder(Node  *pNode, void (*visit) (value_type& item)){
-        if( pNode ){   
-            inorder(pNode->getChild(0), *visit);
-            (*visit)(pNode->getDataRef());
-            inorder(pNode->getChild(1), *visit);
-        }
+
+    // Preorden: Nodo, IZ, DR
+    template <typename F, typename... Args>
+    static void preorder_impl(Node* n, size_t level, F&& f, Args&&... args) {
+        if (!n) return;
+        call_visit(std::forward<F>(f), n, level, std::forward<Args>(args)...);
+        preorder_impl(n->getChild(0), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
+        preorder_impl(n->getChild(1), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
     }
 
-    // Variadic templates (See foreach.h)
-    template <typename Function, typename... Args>
-    void postorder(Function func, Args const&... args)
-    {    postorder(m_pRoot, 0, func, args...);}
-
-    template <typename Function,typename... Args>
-    void postorder(Node* pNode, size_t level, 
-                   Function func, Args const&... args) {
-        if (pNode) {
-            postorder(pNode->getChild(0), level + 1, func, args...);
-            postorder(pNode->getChild(1), level + 1, func, args...);
-            func(pNode, level); 
-        }
-    }
-    // TODO: generalize this function to apply any function
-    void postorder(Node  *pNode, size_t level, ostream &os){
-        if( pNode ){   
-            postorder(pNode->getChild(0), level+1, os);
-            postorder(pNode->getChild(1), level+1, os);
-            os << " --> " << pNode->getDataRef();
-        }
+    // Postorden: IZ, DR, Nodo
+    template <typename F, typename... Args>
+    static void postorder_impl(Node* n, size_t level, F&& f, Args&&... args) {
+        if (!n) return;
+        postorder_impl(n->getChild(0), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
+        postorder_impl(n->getChild(1), level + 1, std::forward<F>(f), std::forward<Args>(args)...);
+        call_visit(std::forward<F>(f), n, level, std::forward<Args>(args)...);
     }
 
-    // TODO: Generalize this function to apply any function
-    void preorder (ostream &os)    {   preorder (m_pRoot, 0, os);  }
-    // TODO: Generalize this function to apply any function
-    void preorder(Node  *pNode, size_t level, ostream &os){
-        if( pNode ){   
-            os << " --> " << pNode->getDataRef();
-            preorder(pNode->getChild(0), level+1, os);
-            preorder(pNode->getChild(1), level+1, os);            
-        }
-    }
 
-    void print    (ostream &os)    {   print    (m_pRoot, 0, os);  }
-    // TODO: generalize this function to apply any function
-    // Google: C++ parameter packs cplusplus
-    void print(Node  *pNode, size_t level, ostream &os){
-        if( pNode ){
-            Node *pParent = pNode->getParent();
-            print(pNode->getChild(1), level+1, os);
-            os << string(" | ") * level << pNode->getDataRef() << "(" << (pParent?to_string(pParent->getData()):"Root") << ")" <<endl;
-            print(pNode->getChild(0), level+1, os);
-        }
-    }
 
-    // TODO: Tip: recorrer el arbol en preorden
-    void Write(ostream &os) { os << *this;  }
-
-    // TODO: Leer en el arbol desde un stream asumiendo que esta en preorden
-    void Read(istream &is)  { /* TODO */  }
 };
 
-// TODO: este operator << debe seguir estando fuera de la clase
-template <typename Traits>
-ostream & operator<<(std::ostream &os, CBinaryTree<Traits> &obj){
-    os << "CBinaryTree with " << obj.size() << " elements.";
-    obj.inorder(os);
-    return os;
-}
+// // TODO: este operator << debe seguir estando fuera de la clase
+// template <typename Traits>
+// ostream & operator<<(std::ostream &os, CBinaryTree<Traits> &obj){
+//     os << "CBinaryTree with " << obj.size() << " elements.";
+//     obj.inorder(os);
+//     return os;
+// }
 
 template <typename Traits>
 istream & operator>>(istream &is, CBinaryTree<Traits> &obj){
