4a8550f9a7aeaed4861d94c93c87913be9cb35e9
Alex Vega <axvg@users.noreply.github.com>
2025-10-14 20:12:40 -0500

    add rotations, update insert

--- avl.h
+++ avl.h
@@ -4,26 +4,35 @@
 #include "binarytree.h"
 
 template <typename Traits>
-class CAVLNode : public CBinaryTreeNode<value_type>{
-public:
-  using value_type = typename Traits::T;
-  using Node       = CBinaryTreeNode<value_type>;
+class CAVLNode : public CBinaryTreeNode<Traits>
+{
+    public:
+        using T          = typename Traits::T;
+        using Node       = CAVLNode<Traits>;
+        using BaseNode   = CBinaryTreeNode<Traits>;
 protected:
     int     m_balanceFactor = 0; // Balance factor for AVL tree
-public:
+
+        template <typename N> // to allow access to private members
+        friend std::string getTreeDisplay(const N* root);
+    public:
+        CAVLNode(BaseNode* parent, const T& value, Ref ref)
+            : CBinaryTreeNode<Traits>(parent, value, ref) {}
+        int getBalanceFactor() const { return m_balanceFactor; }
+        void setBalanceFactor(int bf) { m_balanceFactor = bf; }
 };
 
 template <typename _T>
 struct AVLAscTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
+    using  T          = _T;
+    using  Node       = CBinaryTreeNode<AVLAscTraits<_T>>;
     using  CompareFn  = less<T>;
 };
 
-template <typename _T>
+template <typename Traits>
 struct AVLDescTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
+    using  T          = typename Traits::T;
+    using  Node       = CBinaryTreeNode<AVLDescTraits<T>>;
     using  CompareFn  = greater<T>;
 };
 
@@ -31,23 +40,108 @@ template <typename Traits>
 class CAVLTree : public CBinaryTree<Traits> {
 public:
     using Base       = CBinaryTree<Traits>;
-    using Node       = typename Traits::Node;
-    using value_type = typename Traits::value_type;  
-    using CompareFn  = typename Traits::CompareFn;
-    using Container  = CAVLTree<Traits>;
-    using iterator   = binary_tree_iterator<Container>;
-
+    using Node       = typename Base::Node;
+    using AVLNode    = CAVLNode<Traits>;
+    using T         = typename Base::T;
+    using CompareFn = typename Base::CompareFn;
+    using Container = CAVLTree<Traits>;
+    using iterator  = binary_tree_forward_iterator<Container>;
 protected:
-    // Additional members for AVL tree balancing can be added here
-    Node *internal_insert(value_type &elem, Ref ref,
-                          Node* pParent, Node*& rpOrigin) override
+    Node* createNode(Node* parent, const T& value, Ref ref)
     {
-        // Call base class insert
-        Node* newNode = Base::internal_insert(elem, ref, pParent, rpOrigin);
-        // TODO: Verificar balance y realizar rotaciones si es necesario
-        
-        return newNode;
-    }
+            return new AVLNode(parent, value, ref);
+        }
+
+        int getHeight(Node* node) const
+        {
+            if (node == nullptr) return 0;
+            int leftHeight = getHeight(node->m_children[0]);
+            int rightHeight = getHeight(node->m_children[1]);
+            return 1 + std::max(leftHeight, rightHeight);
+        }
+
+        int getBalance(Node* node) const
+        {
+            if (node == nullptr) return 0;
+            return getHeight(node->m_children[0]) - getHeight(node->m_children[1]);
+        }
+
+        Node* internal_insert(const T& value, Ref ref, Node* parent, Node*& node) override
+        {
+            // paso 1: bst.insert() normal
+            if (node == nullptr) {
+                this->m_size++;
+                AVLNode* newNode = new AVLNode(parent, value, ref);
+                newNode->setBalanceFactor(0);
+                return newNode;
+            }
+
+            size_t branch = this->m_compare(value, node->getValue()) ? 0 : 1;
+            node->m_children[branch] = internal_insert(value, ref, node, node->m_children[branch]);
+
+            // paso 2: actualizar factor de balance
+            AVLNode* avlNode = static_cast<AVLNode*>(node);
+            int balance = getBalance(node);
+            avlNode->setBalanceFactor(balance);
+
+            // paso 3: si el nodo está desbalanceado, realizar rotaciones
+            if (balance > 1 && getBalance(node->m_children[0]) >= 0) { // LL
+                return rotateRight(avlNode);
+            }
+
+            if (balance < -1 && getBalance(node->m_children[1]) <= 0) { // RR
+                return rotateLeft(avlNode);
+            }
+
+            if (balance > 1 && getBalance(node->m_children[0]) < 0) { // LR
+                node->m_children[0] = rotateLeft(static_cast<AVLNode*>(node->m_children[0]));
+                return rotateRight(avlNode);
+            }
+
+            if (balance < -1 && getBalance(node->m_children[1]) > 0) { // RL
+                node->m_children[1] = rotateRight(static_cast<AVLNode*>(node->m_children[1]));
+                return rotateLeft(avlNode);
+            }
+
+            return node;
+        }
+
+        Node* rotateLeft(AVLNode* node)
+        {
+            AVLNode* newRoot = static_cast<AVLNode*>(node->m_children[1]);
+            Node* temp = newRoot->m_children[0];
+
+            newRoot->m_children[0] = node;
+            node->m_children[1] = temp;
+
+            newRoot->m_parent = node->m_parent;
+            node->m_parent = newRoot;
+            if (temp) temp->m_parent = node;
+
+            node->setBalanceFactor(getBalance(node));
+            newRoot->setBalanceFactor(getBalance(newRoot));
+
+            return newRoot;
+        }
+
+        Node* rotateRight(AVLNode* node)
+        {
+            AVLNode* newRoot = static_cast<AVLNode*>(node->m_children[0]);
+            Node* temp = newRoot->m_children[1];
+
+            newRoot->m_children[1] = node;
+            node->m_children[0] = temp;
+            
+            newRoot->m_parent = node->m_parent;
+            node->m_parent = newRoot;
+            if (temp) temp->m_parent = node;
+            
+            node->setBalanceFactor(getBalance(node));
+            newRoot->setBalanceFactor(getBalance(newRoot));
+            
+            return newRoot;
+        }
+
 public:
     CAVLTree() : Base() {} // Empty tree
 
