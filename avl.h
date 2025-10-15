a7a4180d5431405a812bcbf6d8361efd6e9cd84b
MARCELO <gerson.nunez.h@uni.pe>
2025-10-14 20:04:35 -0500

    Avances en 10-AvlTree

--- avl.h
+++ avl.h
@@ -3,54 +3,74 @@
 
 #include "binarytree.h"
 
+//
+// CAVLNode: ahora hereda correctamente de CBinaryTreeNode<Traits>
+// y se le agregó un atributo extra (m_balanceFactor).
+//
 template <typename Traits>
-class CAVLNode : public CBinaryTreeNode<value_type>{
+class CAVLNode : public CBinaryTreeNode<Traits> {
 public:
-  using value_type = typename Traits::T;
-  using Node       = CBinaryTreeNode<value_type>;
+    using value_type = typename Traits::T;
+    using Node       = CAVLNode<Traits>;
+
 protected:
-    int     m_balanceFactor = 0; // Balance factor for AVL tree
+    int m_balanceFactor = 0; // <-- agregado (no existía en el original)
+
 public:
+    // <-- agregado: constructor que llama al de la clase base
+    CAVLNode(Node* pParent, value_type data, Ref ref,
+             Node* p0 = nullptr, Node* p1 = nullptr)
+        : CBinaryTreeNode<Traits>(pParent, data, ref, p0, p1) {}
+};
+
+//
+// Traits corregidos: ahora usan T y CAVLNode con los traits correctos.
+// En el original estaban mal definidos (usaban value_type o T sin traits).
+//
+template <typename _T>
+struct AVLAscTraits {
+    using T         = _T;
+    using Node      = CAVLNode<AVLAscTraits<T>>;
+    using CompareFn = less<T>;
 };
 
 template <typename _T>
-struct AVLAscTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = less<T>;
+struct AVLDescTraits {
+    using T         = _T;
+    using Node      = CAVLNode<AVLDescTraits<T>>;
+    using CompareFn = greater<T>;
 };
 
-template <typename _T>
-struct AVLDescTraits{
-    using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = greater<T>;
-};
-
+//
+// CAVLTree: ahora hereda correctamente de CBinaryTree<Traits>
+// y redefine internal_insert() para luego implementar balanceo.
+// En el original no heredaba ni tenía override.
+//
 template <typename Traits>
 class CAVLTree : public CBinaryTree<Traits> {
 public:
     using Base       = CBinaryTree<Traits>;
     using Node       = typename Traits::Node;
-    using value_type = typename Traits::value_type;  
+    using value_type = typename Traits::T;
     using CompareFn  = typename Traits::CompareFn;
     using Container  = CAVLTree<Traits>;
     using iterator   = binary_tree_iterator<Container>;
 
 protected:
-    // Additional members for AVL tree balancing can be added here
-    Node *internal_insert(value_type &elem, Ref ref,
+    //
+    // internal_insert sobrescrito:
+    // ahora llama al insert base y deja espacio para balancear.
+    //
+    Node* internal_insert(value_type &elem, Ref ref,
                           Node* pParent, Node*& rpOrigin) override
     {
-        // Call base class insert
         Node* newNode = Base::internal_insert(elem, ref, pParent, rpOrigin);
-        // TODO: Verificar balance y realizar rotaciones si es necesario
-        
+        // TODO: agregar rotaciones y cálculo de balance
         return newNode;
     }
-public:
-    CAVLTree() : Base() {} // Empty tree
 
+public:
+    CAVLTree() : Base() {} // Constructor sin cambios
 };
 
 #endif // __AVL_H__
