3629d4f85cdf070e3a62f5353e15aed6753d64d8
Alex Vega <axvg@users.noreply.github.com>
2025-11-05 01:43:40 -0500

    add generic firsthat

--- btree.h
+++ btree.h
@@ -89,6 +89,10 @@ public:
        template<typename Func, typename... Args>
        void            ForEach(Func&& func, Args&&... args)
        {               m_Root.ForEach(std::forward<Func>(func), 0, std::forward<Args>(args)...);}
+
+       template<typename Func, typename... Args>
+       ObjectInfo*     FirstThat(Func&& func, Args&&... args)
+       {               return m_Root.FirstThat(std::forward<Func>(func), 0, std::forward<Args>(args)...);}
 protected:
        BTNode          m_Root;
        size_t          m_Height;  // height of tree
