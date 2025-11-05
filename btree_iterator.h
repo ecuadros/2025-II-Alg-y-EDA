a246ebb91a4461b7b03c42cf59ff7ca4047ffdeb
Alex Vega <axvg@users.noreply.github.com>
2025-11-05 08:39:52 -0500

    add iterator

---
+++ btree_iterator.h
@@ -0,0 +1,107 @@
+#ifndef __BTREE_ITERATOR_H__
+#define __BTREE_ITERATOR_H__
+
+#include "btreepage.h"
+
+template <typename Trait>
+class CBTreePage;
+
+template <typename Trait>
+class btree_iterator
+{
+    friend class BTree<Trait>;
+    friend class CBTreePage<Trait>;
+
+public:
+    using BTPage = CBTreePage<Trait>;
+    using ObjectInfo = typename BTPage::ObjectInfo;
+
+private:
+    // state is a pointer to current node and index within node
+    BTPage* m_node;
+    size_t  m_pos;
+
+    btree_iterator(BTPage* node, size_t pos)
+        : m_node(node), m_pos(pos) {}
+
+public:
+    btree_iterator() : m_node(nullptr), m_pos(0) {}
+
+    ObjectInfo& operator*() const
+    {
+        return m_node->m_Keys[m_pos];
+    }
+
+    ObjectInfo* operator->() const
+    {
+        return &(m_node->m_Keys[m_pos]);
+    }
+
+    // inorder
+    btree_iterator& operator++()
+    {
+        BTPage* right_subtree = m_node->m_SubPages[m_pos + 1];
+        if (right_subtree)
+        {
+            m_node = right_subtree->GetLeftmostLeaf();
+            m_pos = 0;
+        }
+        else
+        {
+            m_pos++;
+
+            if (m_pos < m_node->m_KeyCount)
+            {
+                return *this;
+            }
+
+            BTPage* child = m_node;
+            m_node = m_node->m_Parent;
+
+            while (m_node != nullptr && child == m_node->m_SubPages[m_node->m_KeyCount])
+            {
+                child = m_node;
+                m_node = m_node->m_Parent;
+            }
+
+            if (m_node == nullptr)
+            {
+                m_pos = 0;
+            }
+            else
+            {
+                for (m_pos = 0; m_pos <= m_node->m_KeyCount; m_pos++)
+                {
+                    if (m_node->m_SubPages[m_pos] == child)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        return *this;
+    }
+
+    btree_iterator operator++(int)
+    {
+        btree_iterator old = *this; // copy actual state
+        operator++();
+        return old;                 // return old state
+    }
+
+
+    bool operator==(const btree_iterator& other) const
+    {
+        return m_node == other.m_node && m_pos == other.m_pos;
+    }
+
+    bool operator!=(const btree_iterator& other) const
+    {
+        return !(*this == other);
+    }
+
+    BTPage* getCurrentPage() const { return m_node; }
+    size_t getCurrentIndex() const { return m_pos; }
+};
+
+#endif // __BTREE_ITERATOR_H__
