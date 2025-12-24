18d703952a4412330506097d57bc3e1da231ccdd
AngelloPP <angellojamir@hotmail.com>
2025-12-24 02:24:25 -0500

    fix

--- ArbolB.cpp
+++ ArbolB.cpp
@@ -1,88 +1,116 @@
+//tstbtree.cc  
+//Author: Chicana D�az, Johan Pier
+//Bachelor in Engineering of System
+//#include <iostream.h>
 #include <time.h>
 #include <stdlib.h>
+
 #include "btree.h"
-#include <fstream>
 #include <string>
-#include <vector>
+
+//const char * keys="CDAMPIWNBKEHOLJYQZFXVRTSGU";
+const char * keys1 = "D1XJ2xTg8zKL9AhijOPQcEowRSp0NbW567BUfCqrs4FdtYZakHIuvGV3eMylmn";
+const char * keys2 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz��";
+const char * keys3 = "�DYZakHIUwxVJ203ejOP9Qc8AdtuEop1XvTRghSNbW567BfiCqrs4FGMyz�KLlmn";
 
 const int BTreeSize = 3;
-void DemoRTree() {
-    cout << "\n R-Tree: " << endl;
+int main (int argc, char * argv){
+       int result, i;
+       BTree <char> bt (BTreeSize);
+       for (i = 0; keys1[i]; i++)
+       {
+               //cout<<"Inserting "<<keys1[i]<<endl;
+               result = bt.Insert(keys1[i], i*i);
+               //bt.Print(cout);
+       }
+       bt.Print(cout);
+       /*for (i = 0; keys2[i]; i++)
+       {
+               cout << "Searching " << keys2[i] << " ";
+               long ObjID = bt.Search(keys2[i]);
+               if( ObjID != -1 )
+                       cout << "Achei " << keys2[i] << " ID = " << ObjID << endl;
+               else
+                       cout <<"Nao achei!" << keys2[i] << endl;
+       }*/
+       /*cout.flush();
 
-
-    RTree<RTreeTrait<int, 2>> rtree(BTreeSize);
-    
-    // ejemplo
-    std::vector<std::pair<HyperRect<2>, int>> data = {
-        {{{10, 10}, {20, 20}}, 1}, {{{15, 15}, {25, 25}}, 2}, {{{30, 30}, {40, 40}}, 3},
-        {{{70, 70}, {80, 80}}, 4}, {{{5, 45}, {15, 55}}, 5},  {{{35, 5}, {45, 15}}, 6},
-        {{{80, 10}, {90, 20}}, 7}, {{{10, 80}, {20, 90}}, 8}, {{{50, 50}, {60, 60}}, 9},
-        {{{55, 55}, {65, 65}}, 10},{{{90, 90}, {100, 100}}, 11} 
-    };
-
-    cout << "Rectangulos en el R-Tree" << endl;
-    for (const auto& pair : data) {
-        rtree.Insert(pair.first, pair.second);
-    }
-
-    cout << "Estructura del R-Tree:" << endl;
-    cout << rtree;
-
-    cout << "\n Busqueda en el R-Tree" << endl;
-    HyperRect<2> areaDeBusqueda = {{18, 18}, {35, 35}};
-    cout << "Buscando rectangulos que intersectan con el area: " << areaDeBusqueda << endl;
-
-    std::vector<int> resultados = rtree.Search(areaDeBusqueda);
-
-    cout << "Resultados (IDs): ";
-    for (int id : resultados) { cout << id << " "; }
-    cout << endl;
+       for (i = 0; keys3[i]; i++)
+       {
+               cout << "Removing " << keys3[i] << " ";
+               if( bt.Remove(keys3[i], -1) )
+                       cout << keys3[i] << " removido !" << endl;
+               else
+                       cout <<"Nao achei!" << keys3[i] << endl;
+               bt.Print(cout);
+       }
+       bt.Print(cout);
+       cout.flush();*/
+       return 1;
 }
 
-void DemoRTree_Delete_Read_Write() {
-    cout << "\nBorrado, Escritura y Lectura: " << endl;
-    RTree<RTreeTrait<int, 2>> rtree(BTreeSize);
 
-    std::vector<std::pair<HyperRect<2>, int>> data = {
-        {{{10, 10}, {20, 20}}, 1}, {{{15, 15}, {25, 25}}, 2}, {{{30, 30}, {40, 40}}, 3},
-        {{{70, 70}, {80, 80}}, 4}, {{{5, 45}, {15, 55}}, 5},  {{{35, 5}, {45, 15}}, 6},
-        {{{50, 50}, {60, 60}}, 9}
-    };
-    for (const auto& pair : data) {
-        rtree.Insert(pair.first, pair.second);
-    }
 
-    cout << "Arbol antes de borrar:" << endl;
-    cout << rtree;
 
-    // Borrado
-    HyperRect<2> to_delete_rect = {{50, 50}, {60, 60}};
-    int to_delete_id = 9;
-    cout << "\nBorrando rectangulo con ID " << to_delete_id << " y area " << to_delete_rect << endl;
-    if (rtree.Remove(to_delete_rect, to_delete_id)) {
-        cout << "Borrado exitoso." << endl;
-    } else {
-        cout << "No se pudo borrar." << endl;
-    }
-    cout << "Arbol despues de borrar:" << endl;
-    cout << rtree;
 
-    // Write en disco
-    cout << "\nGuardando arbol en 'rtree.dat'" << endl;
-    ofstream outFile("rtree.dat");
-    rtree.Write(outFile);
-    outFile.close();
-    RTree<RTreeTrait<int, 2>> rtree_loaded(BTreeSize);
-    cout << "Cargando arbol desde 'rtree.dat'..." << endl;
-    ifstream inFile("rtree.dat");
-    rtree_loaded.Read(inFile);
-    inFile.close();
-    cout << "Arbol cargado:" << endl;
-    cout << rtree_loaded;
-}
 
-int main (int argc, char ** argv){
-    DemoRTree();
-    DemoRTree_Delete_Read_Write();
-    return 1;
-}
+
+
+
+/*const char * keys="CDAMPIWNBKEHOLJYQZFXVRTSGU";
+const char * keys2="CDAMPIWNBKEHOLJYQZFXVRTSGU";
+const int BTreeSize = 3;
+main (int argc, char * argv)
+{
+       //__int64 li;
+       BTree <__int64> bt (BTreeSize);
+       for (register int i = 0; i < 1000000; i++)
+       {
+               //cout<<"Inserting "<<keys[i]<<endl;
+               bt.Insert(i, i-1);
+               //bt.Print(cout);
+       }
+
+       for (i = 0; i < 1000; i++)
+       {
+               __int64 key = 975000+(::rand()%50000);
+               //cout << "Searching " << (long)key << " ";
+               long ObjID = bt.Search(key);
+               if( ObjID != -1 )
+                       cout << "Achei " << (long)key << " ID = " << ObjID << endl;
+               else
+                       cout <<"  Nao achei!" << (long)key << endl;
+       }
+       cout.flush();
+
+       return 1;
+}*/
+
+
+
+/*const int BTreeSize = 3;
+main (int argc, char * argv)
+{
+       int result, i;
+       BTree <LONGLONG> bt(BTreeSize);
+       result = bt.Create ("ernesto3-string-btree-start.dat",ios::in|ios::out);
+       if (!result) { cout<<"Please delete testbt.dat"<<endl;return 0; }
+       srand( (unsigned)time( NULL ) );
+       LARGE_INTEGER key;
+       for (i = 0; i < 1000000; i++)
+       {
+               //cout<<"Inserting "<<keys[i]<<endl;
+               char strTmp[50];
+               key.LowPart = rand();
+               key.HighPart = rand();
+               std::string str(strTmp);
+               result = bt.Insert(key.QuadPart, i);
+               //bt.Print(cout);
+               if( i % 100000 == 0 )
+               {       cout << i << endl; cout.flush();        }
+       }
+       //cout << "Searching D " << bt.Search();
+       //bt.Search(1,1);
+       cout.flush();
+       return 1;
+}*/
--- btree.h
+++ btree.h
@@ -1,133 +1,22 @@
 #ifndef __BTREE_H__
 #define __BTREE_H__
 
-/**
- * @file btree.h
- * @brief Define la clase BTree, una estructura de datos de árbol balanceado.
- */
-
-#include <shared_mutex>
 #include <iostream>
-#include <utility>
-#include <mutex> 
-#include <iterator>
-#include <vector>
-#include <fstream>
-#include <array>
+#include "btreepage.h"
 #define DEFAULT_BTREE_ORDER 3
-
-/**
- * @struct HyperRect
- * @brief Representa un hiper-rectángulo en N dimensiones.
- * @tparam Dim El número de dimensiones.
- */
-template <size_t Dim>
-struct HyperRect {
-    std::array<int, Dim> min_coords;
-    std::array<int, Dim> max_coords;
-
-    // calculo el volumen
-    int volume() const {
-        int vol = 1;
-        for (size_t i = 0; i < Dim; ++i) {
-            vol *= (max_coords[i] - min_coords[i]);
-        }
-        return vol;
-    }
-
-    // calculo el volumen de expansión 
-    static int expansionNecesaria(const HyperRect& contenedor, const HyperRect& nuevo) {
-        HyperRect r = unir(contenedor, nuevo);
-        return r.volume() - contenedor.volume();
-    }
-
-    // creo MBR de dos hiperrectangulos.
-    static HyperRect unir(const HyperRect& a, const HyperRect& b) {
-        HyperRect result;
-        for (size_t i = 0; i < Dim; ++i) {
-            result.min_coords[i] = std::min(a.min_coords[i], b.min_coords[i]);
-            result.max_coords[i] = std::max(a.max_coords[i], b.max_coords[i]);
-        }
-        return result;
-    }
-
-    // comprueba si este hiper-rectángulo se solapa con otro
-    bool intersecta(const HyperRect& otro) const {
-        for (size_t i = 0; i < Dim; ++i) {
-            if (max_coords[i] < otro.min_coords[i] || min_coords[i] > otro.max_coords[i]) {
-                return false; // no hay solapamiento y no hay intersección
-            }
-        }
-        return true;
-         // hay solapamiento
-    }
-
-    // comprueba la igualdad exacta 
-    bool operator==(const HyperRect& otro) const {
-        return min_coords == otro.min_coords && max_coords == otro.max_coords;
-    }
-};
-
-template <size_t Dim>
-inline std::ostream& operator<<(std::ostream& os, const HyperRect<Dim>& r) {
-    os << "{";
-    for(size_t i = 0; i < Dim; ++i) os << r.min_coords[i] << (i == Dim - 1 ? "" : ",");
-    os << "},{";
-    for(size_t i = 0; i < Dim; ++i) os << r.max_coords[i] << (i == Dim - 1 ? "" : ",");
-    os << "}";
-    return os;
-};
 
 const size_t MaxHeight = 5; 
 
-/**
- * @struct BTreeTrait
- * @brief Define los tipos y la función de comparación para el B-Tree.
- * @tparam _keyType El tipo de dato para las claves.
- * @tparam _ObjIDType El tipo de dato para los IDs de objeto (valores).
- * @tparam _Compare La función de comparación (ej. std::less o std::greater).
- */
-template <typename _keyType, typename _ObjIDType, typename _Compare = std::less<_keyType>>
+template <typename _keyType, typename _ObjIDType>
 struct BTreeTrait
 {
        using keyType = _keyType;
        using ObjIDType = _ObjIDType;
-       // TODO: agregar funcion de comparacion (DONE)
-       using Compare = _Compare;
+       // TODO: agregar funcion de comparacion
 };
-
-/**
- * @struct BTreeDescTrait
- * @brief Trait de ejemplo para un B-Tree descendente.
- * @tparam _keyType El tipo de dato para las claves.
- * @tparam _ObjIDType El tipo de dato para los IDs de objeto.
- */
-template <typename _keyType, typename _ObjIDType>
-struct BTreeDescTrait : public BTreeTrait<_keyType, _ObjIDType, std::greater<_keyType>> {};
-
-/**
- * @struct RTreeTrait
- * @brief Define los tipos para un R-Tree. La clave es un Rect.
- * @tparam _ObjIDType El tipo de dato para los IDs de objeto.
- * @tparam Dim El número de dimensiones.
- */
-template <typename _ObjIDType, size_t Dim>
-struct RTreeTrait {
-    using keyType   = HyperRect<Dim>; // La clave es un HiperRectángulo
-    using ObjIDType = _ObjIDType; // El ID del objeto
-    struct NoCompare {}; using Compare = NoCompare;
-};
-
-/**
- * @class RTree
- * @brief Implementa una estructura de datos R-Tree, adaptada de una base de B-Tree.
- * @tparam Trait Un struct que define los tipos usados por el R-Tree.
- */
-
-#include "btreepage.h"
 
 template <typename Trait>
-class RTree
+class BTree // this is the full version of the BTree
 {
        typedef typename Trait::keyType    keyType;
        typedef typename Trait::ObjIDType    ObjIDType;
@@ -135,208 +24,82 @@ class RTree
        typedef CBTreePage <Trait> BTNode;// useful shorthand
 
 public:
+       //typedef ObjectInfo iterator;
+       typedef typename BTNode::lpfnForEach2    lpfnForEach2;
+       typedef typename BTNode::lpfnForEach3    lpfnForEach3;
+       typedef typename BTNode::lpfnFirstThat2  lpfnFirstThat2;
+       typedef typename BTNode::lpfnFirstThat3  lpfnFirstThat3;
        typedef typename BTNode::ObjectInfo      ObjectInfo;
 
 public:
-       /**
-        * @brief Constructor del R-Tree.
-        * @param order El orden del árbol.
-        * @param unique Verdadero si las claves deben ser únicas.
-        */
-public:
-       RTree(size_t order = DEFAULT_BTREE_ORDER, bool unique = true)
+       BTree(size_t order = DEFAULT_BTREE_ORDER, bool unique = true)
               : m_Order(order),
                 m_Root(2 * order  + 1, unique),
-                m_NumKeys(0),
-                m_Unique(unique)
+                m_Unique(unique),
+                m_NumKeys(0)
        {
               m_Root.SetMaxKeysForChilds(order);
               m_Height = 1;
        }
-       /**
-        * @brief Constructor por movimiento
-        * @param other El R-Tree a mover.
-        */
-       RTree(RTree&& other) noexcept
-       {
-              std::scoped_lock lock(m_Mutex, other.m_Mutex);
- 
-              m_Root    = std::exchange(other.m_Root, BTNode(0, false));
-              m_Height  = std::exchange(other.m_Height, 1);
-              m_Order   = std::exchange(other.m_Order, 0);
-              m_NumKeys = std::exchange(other.m_NumKeys, 0);
-              m_Unique  = std::exchange(other.m_Unique, false);
-       }
-       /// @brief Destructor.
-       ~RTree() {}
+       ~BTree() {}
        //int           Open (char * name, int mode);
        //int           Create (char * name, int mode);
        //int           Close ();
-       /// @brief Escribe el árbol a un flujo de salida para serialización.
-       /// @param os El flujo de salida.
-       void            Write(ostream& os);
-       /// @brief Lee el árbol desde un flujo de entrada para deserialización.
-       /// @param is El flujo de entrada.
-       void            Read(istream& is);
-
-       /**
-        * @brief Inserta un par clave-valor en el árbol.
-        * @param key La clave a insertar.
-        * @param ObjID El valor (ID de objeto) a asociar con la clave.
-        * @return Verdadero si la inserción fue exitosa, falso si la clave era un duplicado.
-        */
-       bool            Insert (const keyType key, const ObjIDType ObjID);
-       /**
-        * @brief Elimina una clave del árbol.
-        * @param key La clave a eliminar.
-        * @param ObjID El ID de objeto asociado (actualmente no se usa en la lógica de eliminación).
-        * @return Verdadero si la eliminación fue exitosa.
-        */
-       bool            Remove (const keyType key, const ObjIDType ObjID);
-       /**
-        * @brief Busca una clave en el árbol.
-        * @param key La clave a buscar.
-        * @return Devolver una lista de resultados.
-        */
-       std::vector<ObjIDType> Search(const keyType& areaBusqueda);
-
-       /// Devuelve el número total de claves en el árbol.
-       size_t            size()  const { std::shared_lock<std::shared_mutex> lock(m_Mutex); return m_NumKeys; }
-       /// Devuelve la altura del árbol.
-       size_t            height() const { std::shared_lock<std::shared_mutex> lock(m_Mutex); return m_Height;      }
-       /// Devuelve el orden del árbol.
-       size_t            GetOrder() const { std::shared_lock<std::shared_mutex> lock(m_Mutex); return m_Order;     }
-
-       /// Imprime la estructura del árbol en un flujo de salida.
-       void            Print (ostream &os) const
-       {               
-              std::shared_lock<std::shared_mutex> lock(m_Mutex);
-              m_Root.Print(os);
+       bool            Insert (const keyType key, const long ObjID);
+       bool            Remove (const keyType key, const long ObjID);
+       ObjIDType       Search (const keyType key)
+       {      ObjIDType ObjID = -1;
+              m_Root.Search(key, ObjID);
+              return ObjID;
        }
+       size_t            size()  { return m_NumKeys; }
+       size_t            height() { return m_Height;      }
+       size_t            GetOrder() { return m_Order;     }
 
-       /**
-        * @brief Aplica una función a cada elemento del árbol en orden.
-        * @tparam Func El tipo de la función.
-        * @tparam Args Los tipos de los argumentos adicionales para la función.
-        * @param level Nivel inicial para el recorrido (usualmente 0).
-        * @param func La función a aplicar.
-        * @param args Argumentos adicionales para la función.
-        */
-       template<typename Func, typename... Args>
-       void ForEach(size_t level, Func&& func, Args&&... args) {
-              m_Root.ForEach(level, std::forward<Func>(func), std::forward<Args>(args)...);
-       }
+       void            Print (ostream &os)
+       {               m_Root.Print(os);                              }
+       void            ForEach( lpfnForEach2 lpfn, void *pExtra1 )
+       {               m_Root.ForEach(lpfn, 0, pExtra1);              }
+       void            ForEach( lpfnForEach3 lpfn, void *pExtra1, void *pExtra2)
+       {               m_Root.ForEach(lpfn, 0, pExtra1, pExtra2);     }
+       ObjectInfo*     FirstThat( lpfnFirstThat2 lpfn, void *pExtra1 )
+       {               return m_Root.FirstThat(lpfn, 0, pExtra1);     }
+       ObjectInfo*     FirstThat( lpfnFirstThat3 lpfn, void *pExtra1, void *pExtra2)
+       {               return m_Root.FirstThat(lpfn, 0, pExtra1, pExtra2);   }
+       //typedef               ObjectInfo iterator;
 
-       /**
-        * @brief Busca el primer elemento que satisface una condición.
-        * @return Un puntero al ObjectInfo si se encuentra, de lo contrario nullptr.
-        */
-       template<typename Func, typename... Args>
-       ObjectInfo* FirstThat(size_t level, Func&& func, Args&&... args) {
-              return m_Root.FirstThat(level, std::forward<Func>(func), std::forward<Args>(args)...);
-       }
 protected:
-       BTNode          m_Root;    ///< El nodo raíz del B-Tree.
-       size_t          m_Height;  ///< Altura del árbol.
-       size_t          m_Order;   ///< Orden del árbol.
-       size_t          m_NumKeys; ///< Número total de claves en el árbol.
-       bool            m_Unique;  ///< Verdadero si las claves deben ser únicas.
-       mutable std::shared_mutex m_Mutex; ///< Mutex de lectura-escritura para seguridad en hilos.
+       BTNode          m_Root;
+       size_t          m_Height;  // height of tree
+       size_t          m_Order;   // order of tree
+       size_t          m_NumKeys; // number of keys
+       bool            m_Unique;  // Accept the elements only once ?
 };     
 
 template <typename Trait>
-bool RTree<Trait>::Insert(const keyType key, const ObjIDType ObjID){
-       std::lock_guard<std::shared_mutex> lock(m_Mutex);
-       
-       BTNode* pNewNode = nullptr;
-       bt_ErrorCode error = m_Root.Insert(key, ObjID, &pNewNode);
-
+bool BTree<Trait>::Insert(const keyType key, const long ObjID){
+       bt_ErrorCode error = m_Root.Insert(key, ObjID);
+       if( error == bt_duplicate )
+               return false;
        m_NumKeys++;
-
        if( error == bt_overflow ){
-               BTNode* pLeftChild = new BTNode(std::move(m_Root));
-               
-               // reinicializar m_Root
-               m_Root.m_MaxKeys = 2 * m_Order + 1;
-               m_Root.m_Unique = m_Unique;
-               m_Root.Create();
-               m_Root.SetMaxKeysForChilds(m_Order);
-
-               // el nodo es el hijo derecho.
-               BTNode* pRightChild = pNewNode;
-
-               // establecer la nueva raíz como padre de los hijos
-               pLeftChild->SetParent(&m_Root);
-               pRightChild->SetParent(&m_Root);
-               m_Root.AddChild(pLeftChild);
-               m_Root.AddChild(pRightChild);
+               m_Root.SplitRoot();
                m_Height++;
        }
        return true;
 }
 
 template <typename Trait>
-std::vector<typename Trait::ObjIDType> RTree<Trait>::Search(const keyType& areaBusqueda)
+bool BTree<Trait>::Remove (const keyType key, const long ObjID)
 {
-    std::shared_lock<std::shared_mutex> lock(m_Mutex);
-    std::vector<ObjIDType> resultados;
-    m_Root.Search(areaBusqueda, resultados);
-    return resultados;
-}
+       bt_ErrorCode error = m_Root.Remove(key, ObjID);
+       if( error == bt_duplicate || error == bt_nofound )
+               return false;
+       m_NumKeys--;
 
-template <typename Trait>
-void RTree<Trait>::Write(ostream& os) {
-    std::lock_guard<std::shared_mutex> lock(m_Mutex);
-    os << m_Order << "\n";
-    os << m_Unique << "\n";
-    os << m_NumKeys << "\n";
-    os << m_Height << "\n";
-    m_Root.Write(os);
-}
-
-template <typename Trait>
-void RTree<Trait>::Read(istream& is) {
-    std::lock_guard<std::shared_mutex> lock(m_Mutex);
-    is >> m_Order >> m_Unique >> m_NumKeys >> m_Height;
-
-    m_Root.m_MaxKeys = 2 * m_Order + 1;
-    m_Root.m_Unique = m_Unique;
-    m_Root.Create();
-
-    m_Root.Read(is);
-}
-
-template <typename Trait>
-bool RTree<Trait>::Remove (const keyType key, const ObjIDType ObjID)
-{
-    std::lock_guard<std::shared_mutex> lock(m_Mutex);
-    
-    std::vector<ObjectInfo> reinsert_list;
-    bool found = m_Root.Remove(key, ObjID, reinsert_list);
-
-    if (!found) return false;
-
-    m_NumKeys--;
-
-    // reinsertar los nodos que tuvieron underflow
-    for (const auto& entry : reinsert_list) {
-        Insert(entry.key, entry.ObjID);
-    }
-
-    // si hay solo 1 hijo, se convierte en la nueva raíz
-    if (m_Root.m_KeyCount == 1 && m_Height > 1 && m_Root.m_SubPages[0] != nullptr) {
-        m_Root = std::move(*m_Root.m_SubPages[0]);
-        m_Height--;
-    }
-
-    return true;
-}
-
-/// Sobrecarga del operador << para imprimir el R-Tree.
-template <typename Trait>
-std::ostream& operator<<(std::ostream& os, const RTree<Trait>& tree) {
-    tree.Print(os);
-    return os;
+       if( error == bt_rootmerged )
+               m_Height--;
+       return true;
 }
 
 #endif
