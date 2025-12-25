7fd27d2bb0e69f92779d5eff2f3728a54f214002
ecuadros <ecuadros@spc.org.pe>
2025-12-25 12:39:48 -0500

    RTree

---
+++ RTree.h
@@ -0,0 +1,236 @@
+#ifndef __RTREE_H__
+#define __RTREE_H__
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <mutex>
+#include "RTreeNode.h"
+
+#define DEFAULT_RTREE_MAX_ENTRIES 4
+
+template <typename _CoordType, typename _ObjIDType>
+struct RTreeTrait {
+    using CoordType = _CoordType;
+    using ObjIDType = _ObjIDType;
+};
+
+template <typename Trait>
+class CRTree {
+    typedef typename Trait::CoordType CoordType;
+    typedef typename Trait::ObjIDType ObjIDType;
+    typedef CRTreeNode<Trait> RTNode;
+
+public:
+    CRTree(size_t maxEntries = DEFAULT_RTREE_MAX_ENTRIES);
+    ~CRTree();
+
+    bool Insert(const Rectangle<CoordType>& rect, const ObjIDType objID);
+    bool Remove(const Rectangle<CoordType>& rect, const ObjIDType objID);
+    void RangeQuery(const Rectangle<CoordType>& range, std::vector<ObjIDType>& results);
+
+    bool WriteToFile(const std::string& filename);
+    bool ReadFromFile(const std::string& filename);
+
+    size_t GetHeight() const {
+        std::lock_guard<std::mutex> lock(m_Mutex);
+        return m_Height;
+    }
+    size_t GetSize() const {
+        std::lock_guard<std::mutex> lock(m_Mutex);
+        return m_Size;
+    }
+
+protected:
+    RTNode* m_Root;
+    size_t m_Height;
+    size_t m_MaxEntries;
+    size_t m_Size;
+    mutable std::mutex m_Mutex;
+
+    void WriteNode(std::ofstream& ofs, RTNode* node);
+    RTNode* ReadNode(std::ifstream& ifs, bool isLeaf);
+    void DestroyTree(RTNode* node);
+};
+
+template <typename Trait>
+CRTree<Trait>::CRTree(size_t maxEntries)
+    : m_Root(nullptr), m_Height(1), m_MaxEntries(maxEntries), m_Size(0) {
+    m_Root = new RTNode(maxEntries, true);
+}
+
+template <typename Trait>
+CRTree<Trait>::~CRTree() {
+    DestroyTree(m_Root);
+}
+
+template <typename Trait>
+void CRTree<Trait>::DestroyTree(RTNode* node) {
+    if (node) {
+        delete node;
+    }
+}
+
+template <typename Trait>
+bool CRTree<Trait>::Insert(const Rectangle<CoordType>& rect, const ObjIDType objID) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+
+    rt_ErrorCode error = m_Root->Insert(rect, objID);
+
+    if (error == rt_overflow) {
+        RTNode* newRoot = new RTNode(m_MaxEntries, false);
+        RTNode* newNode = nullptr;
+
+        m_Root->SplitNode(newNode);
+
+        newRoot->m_Children[0] = m_Root;
+        newRoot->m_Entries[0] = tagRTreeEntry<CoordType, ObjIDType>(m_Root->GetMBR(), -1);
+        newRoot->m_Count++;
+
+        if (newNode) {
+            newRoot->m_Children[1] = newNode;
+            newRoot->m_Entries[1] = tagRTreeEntry<CoordType, ObjIDType>(newNode->GetMBR(), -1);
+            newRoot->m_Count++;
+        }
+
+        newRoot->UpdateMBR();
+        m_Root = newRoot;
+        m_Height++;
+    }
+
+    if (error != rt_duplicate) {
+        m_Size++;
+        return true;
+    }
+
+    return false;
+}
+
+template <typename Trait>
+bool CRTree<Trait>::Remove(const Rectangle<CoordType>& rect, const ObjIDType objID) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+
+    rt_ErrorCode error = m_Root->Remove(rect, objID);
+
+    if (error == rt_ok || error == rt_underflow) {
+        m_Size--;
+        return true;
+    }
+
+    return false;
+}
+
+template <typename Trait>
+void CRTree<Trait>::RangeQuery(const Rectangle<CoordType>& range, std::vector<ObjIDType>& results) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+    m_Root->RangeQuery(range, results);
+}
+
+template <typename Trait>
+bool CRTree<Trait>::WriteToFile(const std::string& filename) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+
+    std::ofstream ofs(filename, std::ios::binary);
+    if (!ofs.is_open()) {
+        return false;
+    }
+
+    ofs.write(reinterpret_cast<const char*>(&m_Height), sizeof(m_Height));
+    ofs.write(reinterpret_cast<const char*>(&m_MaxEntries), sizeof(m_MaxEntries));
+    ofs.write(reinterpret_cast<const char*>(&m_Size), sizeof(m_Size));
+
+    WriteNode(ofs, m_Root);
+
+    ofs.close();
+    return true;
+}
+
+template <typename Trait>
+void CRTree<Trait>::WriteNode(std::ofstream& ofs, RTNode* node) {
+    if (!node) {
+        bool isNull = true;
+        ofs.write(reinterpret_cast<const char*>(&isNull), sizeof(isNull));
+        return;
+    }
+
+    bool isNull = false;
+    ofs.write(reinterpret_cast<const char*>(&isNull), sizeof(isNull));
+
+    ofs.write(reinterpret_cast<const char*>(&node->m_IsLeaf), sizeof(node->m_IsLeaf));
+    ofs.write(reinterpret_cast<const char*>(&node->m_Count), sizeof(node->m_Count));
+
+    for (size_t i = 0; i < node->m_Count; i++) {
+        ofs.write(reinterpret_cast<const char*>(&node->m_Entries[i].mbr),
+                 sizeof(Rectangle<CoordType>));
+        ofs.write(reinterpret_cast<const char*>(&node->m_Entries[i].objID),
+                 sizeof(ObjIDType));
+    }
+
+    if (!node->m_IsLeaf) {
+        for (size_t i = 0; i <= node->m_Count; i++) {
+            WriteNode(ofs, node->m_Children[i]);
+        }
+    }
+}
+
+template <typename Trait>
+bool CRTree<Trait>::ReadFromFile(const std::string& filename) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+
+    std::ifstream ifs(filename, std::ios::binary);
+    if (!ifs.is_open()) {
+        return false;
+    }
+
+    DestroyTree(m_Root);
+
+    ifs.read(reinterpret_cast<char*>(&m_Height), sizeof(m_Height));
+    ifs.read(reinterpret_cast<char*>(&m_MaxEntries), sizeof(m_MaxEntries));
+    ifs.read(reinterpret_cast<char*>(&m_Size), sizeof(m_Size));
+
+    bool isLeaf = (m_Height == 1);
+    m_Root = ReadNode(ifs, isLeaf);
+
+    ifs.close();
+    return true;
+}
+
+template <typename Trait>
+CRTreeNode<Trait>* CRTree<Trait>::ReadNode(std::ifstream& ifs, bool) {
+    bool isNull;
+    ifs.read(reinterpret_cast<char*>(&isNull), sizeof(isNull));
+
+    if (isNull) {
+        return nullptr;
+    }
+
+    bool nodeIsLeaf;
+    size_t count;
+
+    ifs.read(reinterpret_cast<char*>(&nodeIsLeaf), sizeof(nodeIsLeaf));
+    ifs.read(reinterpret_cast<char*>(&count), sizeof(count));
+
+    RTNode* node = new RTNode(m_MaxEntries, nodeIsLeaf);
+    node->m_Count = count;
+
+    for (size_t i = 0; i < count; i++) {
+        Rectangle<CoordType> mbr;
+        ObjIDType objID;
+
+        ifs.read(reinterpret_cast<char*>(&mbr), sizeof(Rectangle<CoordType>));
+        ifs.read(reinterpret_cast<char*>(&objID), sizeof(ObjIDType));
+
+        node->m_Entries[i] = tagRTreeEntry<CoordType, ObjIDType>(mbr, objID);
+    }
+
+    if (!nodeIsLeaf) {
+        for (size_t i = 0; i <= count; i++) {
+            node->m_Children[i] = ReadNode(ifs, false);
+        }
+    }
+
+    node->UpdateMBR();
+    return node;
+}
+
+#endif
---
+++ RTreeNode.h
@@ -0,0 +1,346 @@
+#ifndef __RTREENODE_H__
+#define __RTREENODE_H__
+
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <cmath>
+
+template <typename Trait>
+class CRTree;
+
+enum rt_ErrorCode {rt_ok, rt_overflow, rt_underflow, rt_duplicate, rt_nofound};
+
+template <typename CoordType>
+struct Rectangle {
+    CoordType min_x, min_y;
+    CoordType max_x, max_y;
+
+    Rectangle() : min_x(0), min_y(0), max_x(0), max_y(0) {}
+
+    Rectangle(CoordType x1, CoordType y1, CoordType x2, CoordType y2)
+        : min_x(x1), min_y(y1), max_x(x2), max_y(y2) {}
+
+    CoordType area() const {
+        return (max_x - min_x) * (max_y - min_y);
+    }
+
+    CoordType enlargement(const Rectangle& other) const {
+        CoordType new_min_x = std::min(min_x, other.min_x);
+        CoordType new_min_y = std::min(min_y, other.min_y);
+        CoordType new_max_x = std::max(max_x, other.max_x);
+        CoordType new_max_y = std::max(max_y, other.max_y);
+        return (new_max_x - new_min_x) * (new_max_y - new_min_y) - area();
+    }
+
+    bool intersects(const Rectangle& other) const {
+        return !(max_x < other.min_x || min_x > other.max_x ||
+                max_y < other.min_y || min_y > other.max_y);
+    }
+
+    bool contains(const Rectangle& other) const {
+        return min_x <= other.min_x && max_x >= other.max_x &&
+               min_y <= other.min_y && max_y >= other.max_y;
+    }
+
+    void expand(const Rectangle& other) {
+        min_x = std::min(min_x, other.min_x);
+        min_y = std::min(min_y, other.min_y);
+        max_x = std::max(max_x, other.max_x);
+        max_y = std::max(max_y, other.max_y);
+    }
+};
+
+template <typename CoordType, typename ObjIDType>
+struct tagRTreeEntry {
+    Rectangle<CoordType> mbr;
+    ObjIDType objID;
+
+    tagRTreeEntry() : objID(-1) {}
+    tagRTreeEntry(const Rectangle<CoordType>& rect, ObjIDType id)
+        : mbr(rect), objID(id) {}
+};
+
+template <typename Trait>
+class CRTreeNode {
+    friend class CRTree<Trait>;
+    typedef typename Trait::CoordType CoordType;
+    typedef typename Trait::ObjIDType ObjIDType;
+    typedef CRTreeNode<Trait> RTNode;
+    typedef tagRTreeEntry<CoordType, ObjIDType> Entry;
+
+public:
+    CRTreeNode(size_t maxEntries, bool isLeaf = true);
+    ~CRTreeNode();
+
+    rt_ErrorCode Insert(const Rectangle<CoordType>& rect, const ObjIDType objID);
+    rt_ErrorCode Remove(const Rectangle<CoordType>& rect, const ObjIDType objID);
+    void RangeQuery(const Rectangle<CoordType>& range, std::vector<ObjIDType>& results);
+
+    Rectangle<CoordType> GetMBR() const;
+    void UpdateMBR();
+
+protected:
+    size_t m_MaxEntries;
+    size_t m_MinEntries;
+    size_t m_Count;
+    bool m_IsLeaf;
+
+    Rectangle<CoordType> m_MBR;
+    std::vector<Entry> m_Entries;
+    std::vector<RTNode*> m_Children;
+
+    bool IsLeaf() const { return m_IsLeaf; }
+    bool IsOverflow() const { return m_Count > m_MaxEntries; }
+    bool IsUnderflow() const { return m_Count < m_MinEntries; }
+
+    size_t ChooseSubtree(const Rectangle<CoordType>& rect);
+    void SplitNode(RTNode*& newNode);
+    void PickSeeds(size_t& seed1, size_t& seed2);
+};
+
+template <typename Trait>
+CRTreeNode<Trait>::CRTreeNode(size_t maxEntries, bool isLeaf)
+    : m_MaxEntries(maxEntries), m_Count(0), m_IsLeaf(isLeaf) {
+    m_MinEntries = maxEntries / 2;
+    m_Entries.resize(maxEntries + 1);
+    m_Children.resize(maxEntries + 2, nullptr);
+}
+
+template <typename Trait>
+CRTreeNode<Trait>::~CRTreeNode() {
+    if (!m_IsLeaf) {
+        for (size_t i = 0; i <= m_Count; i++) {
+            delete m_Children[i];
+        }
+    }
+}
+
+template <typename Trait>
+Rectangle<typename Trait::CoordType> CRTreeNode<Trait>::GetMBR() const {
+    return m_MBR;
+}
+
+template <typename Trait>
+void CRTreeNode<Trait>::UpdateMBR() {
+    if (m_Count == 0) {
+        m_MBR = Rectangle<CoordType>();
+        return;
+    }
+
+    m_MBR = m_Entries[0].mbr;
+    for (size_t i = 1; i < m_Count; i++) {
+        m_MBR.expand(m_Entries[i].mbr);
+    }
+
+    if (!m_IsLeaf) {
+        for (size_t i = 0; i <= m_Count; i++) {
+            if (m_Children[i]) {
+                m_MBR.expand(m_Children[i]->GetMBR());
+            }
+        }
+    }
+}
+
+template <typename Trait>
+size_t CRTreeNode<Trait>::ChooseSubtree(const Rectangle<CoordType>& rect) {
+    if (m_IsLeaf) {
+        return m_Count;
+    }
+
+    size_t best = 0;
+    CoordType minEnlargement = m_Children[0]->GetMBR().enlargement(rect);
+
+    for (size_t i = 1; i <= m_Count; i++) {
+        CoordType enlargement = m_Children[i]->GetMBR().enlargement(rect);
+        if (enlargement < minEnlargement) {
+            minEnlargement = enlargement;
+            best = i;
+        }
+    }
+
+    return best;
+}
+
+template <typename Trait>
+rt_ErrorCode CRTreeNode<Trait>::Insert(const Rectangle<CoordType>& rect, const ObjIDType objID) {
+    if (m_IsLeaf) {
+        m_Entries[m_Count] = Entry(rect, objID);
+        m_Count++;
+        UpdateMBR();
+
+        if (IsOverflow()) {
+            return rt_overflow;
+        }
+        return rt_ok;
+    }
+
+    size_t index = ChooseSubtree(rect);
+    rt_ErrorCode error = m_Children[index]->Insert(rect, objID);
+
+    if (error == rt_overflow) {
+        RTNode* newNode = nullptr;
+        m_Children[index]->SplitNode(newNode);
+
+        if (newNode) {
+            for (size_t i = m_Count; i > index; i--) {
+                m_Entries[i] = m_Entries[i - 1];
+                m_Children[i + 1] = m_Children[i];
+            }
+            m_Entries[index] = Entry(newNode->GetMBR(), -1);
+            m_Children[index + 1] = newNode;
+            m_Count++;
+        }
+
+        UpdateMBR();
+
+        if (IsOverflow()) {
+            return rt_overflow;
+        }
+    }
+
+    UpdateMBR();
+    return rt_ok;
+}
+
+template <typename Trait>
+void CRTreeNode<Trait>::PickSeeds(size_t& seed1, size_t& seed2) {
+    CoordType maxWaste = -1;
+    seed1 = 0;
+    seed2 = 1;
+
+    for (size_t i = 0; i <= m_Count; i++) {
+        for (size_t j = i + 1; j <= m_Count; j++) {
+            Rectangle<CoordType> combined = m_Entries[i].mbr;
+            combined.expand(m_Entries[j].mbr);
+            CoordType waste = combined.area() - m_Entries[i].mbr.area() - m_Entries[j].mbr.area();
+
+            if (waste > maxWaste) {
+                maxWaste = waste;
+                seed1 = i;
+                seed2 = j;
+            }
+        }
+    }
+}
+
+template <typename Trait>
+void CRTreeNode<Trait>::SplitNode(RTNode*& newNode) {
+    newNode = new RTNode(m_MaxEntries, m_IsLeaf);
+
+    size_t seed1, seed2;
+    PickSeeds(seed1, seed2);
+
+    std::vector<Entry> tempEntries;
+    std::vector<RTNode*> tempChildren;
+
+    for (size_t i = 0; i <= m_Count; i++) {
+        tempEntries.push_back(m_Entries[i]);
+        if (!m_IsLeaf) {
+            tempChildren.push_back(m_Children[i]);
+        }
+    }
+
+    m_Count = 0;
+    m_Entries[m_Count++] = tempEntries[seed1];
+    if (!m_IsLeaf) {
+        m_Children[0] = tempChildren[seed1];
+    }
+
+    newNode->m_Entries[newNode->m_Count++] = tempEntries[seed2];
+    if (!m_IsLeaf) {
+        newNode->m_Children[0] = tempChildren[seed2];
+    }
+
+    for (size_t i = 0; i < tempEntries.size(); i++) {
+        if (i == seed1 || i == seed2) continue;
+
+        Rectangle<CoordType> mbr1 = GetMBR();
+        Rectangle<CoordType> mbr2 = newNode->GetMBR();
+
+        CoordType enl1 = mbr1.enlargement(tempEntries[i].mbr);
+        CoordType enl2 = mbr2.enlargement(tempEntries[i].mbr);
+
+        if (enl1 < enl2) {
+            m_Entries[m_Count] = tempEntries[i];
+            if (!m_IsLeaf) {
+                m_Children[m_Count] = tempChildren[i];
+            }
+            m_Count++;
+        } else {
+            newNode->m_Entries[newNode->m_Count] = tempEntries[i];
+            if (!m_IsLeaf) {
+                newNode->m_Children[newNode->m_Count] = tempChildren[i];
+            }
+            newNode->m_Count++;
+        }
+
+        UpdateMBR();
+        newNode->UpdateMBR();
+    }
+}
+
+template <typename Trait>
+rt_ErrorCode CRTreeNode<Trait>::Remove(const Rectangle<CoordType>& rect, const ObjIDType objID) {
+    if (!m_MBR.intersects(rect)) {
+        return rt_nofound;
+    }
+
+    if (m_IsLeaf) {
+        for (size_t i = 0; i < m_Count; i++) {
+            if (m_Entries[i].objID == objID &&
+                m_Entries[i].mbr.min_x == rect.min_x &&
+                m_Entries[i].mbr.min_y == rect.min_y &&
+                m_Entries[i].mbr.max_x == rect.max_x &&
+                m_Entries[i].mbr.max_y == rect.max_y) {
+
+                for (size_t j = i; j < m_Count - 1; j++) {
+                    m_Entries[j] = m_Entries[j + 1];
+                }
+                m_Count--;
+                UpdateMBR();
+
+                if (IsUnderflow()) {
+                    return rt_underflow;
+                }
+                return rt_ok;
+            }
+        }
+        return rt_nofound;
+    }
+
+    for (size_t i = 0; i <= m_Count; i++) {
+        if (m_Children[i]) {
+            rt_ErrorCode error = m_Children[i]->Remove(rect, objID);
+            if (error != rt_nofound) {
+                UpdateMBR();
+                return error;
+            }
+        }
+    }
+
+    return rt_nofound;
+}
+
+template <typename Trait>
+void CRTreeNode<Trait>::RangeQuery(const Rectangle<CoordType>& range, std::vector<ObjIDType>& results) {
+    if (!m_MBR.intersects(range)) {
+        return;
+    }
+
+    if (m_IsLeaf) {
+        for (size_t i = 0; i < m_Count; i++) {
+            if (m_Entries[i].mbr.intersects(range)) {
+                results.push_back(m_Entries[i].objID);
+            }
+        }
+    } else {
+        for (size_t i = 0; i <= m_Count; i++) {
+            if (m_Children[i]) {
+                m_Children[i]->RangeQuery(range, results);
+            }
+        }
+    }
+}
+
+#endif
